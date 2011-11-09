9605439e98cfb11b5413775354c8723a7599c27d
nojhan <nojhan@gmail.com>
2011-11-09 14:47:11 +0100

    scripts to add metadata to the PDF produced by inkscape

---
+++ insert_metada.sh
@@ -0,0 +1,5 @@
+#bin/sh
+
+pdftk geometromap_solarized.pdf update_info meta.data output geometromap_solarized_ok.pdf
+pdftk geometromap_solarized_clair.pdf update_info meta.data output geometromap_solarized_clair_ok.pdf
+
